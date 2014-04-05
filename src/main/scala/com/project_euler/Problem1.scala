989356e8004a744496aebd754ed87c518a1683c0
Bharat Ravisekar <bravisekar@stumbleupon.com>
2014-04-04 17:01:29 -0700

    New Project
    Solution to Problem1

---
+++ src/main/scala/com/project_euler/Problem1.scala
@@ -0,0 +1,13 @@
+package com.project_euler
+
+/**
+ * Sum of multiples of 3 or 5 under 1000.
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * @author Bharat Ravisekar
+ */
+object Problem1 extends App {
+  val result = (1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).sum
+  println(result)
+}
