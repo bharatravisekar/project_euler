63106f781c24e7653c8b30e11191dd689a3b167e
Bharat Ravisekar <bravisekar@stumbleupon.com>
2014-04-05 20:54:03 -0700

    Problem 10 solution.

---
+++ src/main/scala/com/project_euler/Problem10.scala
@@ -0,0 +1,26 @@
+package com.project_euler
+
+/**
+ * Summation of primes.
+ *
+ * @author Bharat Ravisekar
+ */
+object Problem10 extends App {
+  private val N = 2000000
+
+  private def isPrime(n: Int, d: Int = 2): Boolean = {
+    if (n == 1)
+      false
+    else if (d > n/2)
+      true
+    else if (n % d == 0)
+      false
+    else
+      isPrime(n, d + 1)
+  }
+
+  private val result =
+    (1 to N).foldLeft(0L){ (sum, number) => sum + (if(isPrime(number)) number else 0) }
+
+  println(result)
+}
